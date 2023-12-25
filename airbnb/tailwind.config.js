225cdc250b082b922aa4107e7a6528b720292d74
Sairam-50 <sairams.shinrai74@gmail.com>
2023-12-25 19:42:14 +0530

    Revert "done for the day"
    This reverts commit 477e8e0b616fcf55b3ad3b96823f194336f03d40.

--- airbnb/tailwind.config.js
+++ airbnb/tailwind.config.js
@@ -5,26 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      width:{
-          "550":"550px",
-      },
-      height:{
-        "70":"70px",
-      },
-      dropShadow:{
-        "drop-shadow-custom":" drop-shadow(0px 2.983px 7.456px rgba(0, 0, 0, 0.10))",
-      },
-      fill:{
-        "fill-custom":"#FFFFFF",
-      },
-      fontFamily:{
-        "Poppins":"Poppins",
-      },
-      backgroundColor:{
-        "bg-custom":"#FFFFFF",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
